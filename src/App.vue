7fc0b370dd3a5610970c9c03489007249ff48057
Rhajaina Agent <dev@stromdao.com>
2026-04-05 11:52:43 +0000

    fix: remove prop binding for assets modal to prevent vue hydration error

--- src/App.vue
+++ src/App.vue
@@ -262,7 +262,6 @@ const currentKpi = computed(() => {
   <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0" enter-to-class="opacity-100" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100" leave-to-class="opacity-0">
     <ModalAssetTable 
       v-if="znpStore.activeModal === 'assets'" 
-      :assets="znpStore.assets" 
       @close="znpStore.closeModal()" 
     />
   </transition>
